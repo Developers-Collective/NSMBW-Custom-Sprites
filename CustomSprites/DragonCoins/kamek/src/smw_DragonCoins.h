e34bf6d15e3ec5579d4bc2078478d2d08274420c
wakanameko <63937252+wakanameko@users.noreply.github.com>
2024-11-14 20:37:33 +0900

    Update smw_DragonCoins.h

--- CustomSprites/DragonCoins/kamek/src/smw_DragonCoins.h
+++ CustomSprites/DragonCoins/kamek/src/smw_DragonCoins.h
@@ -7,6 +7,8 @@
 #include <sfx.h>
 #include <profile.h>
 
+extern "C" void addRest(int playerIDRest, int howMany, bool reallyAddRest);	// reallyAddRest is always true
+extern "C" void incRestAll(bool reallyAddRest);	// reallyAddRest is always true
 extern "C" void oneUPPopup(Vec pos);   // set the position (x,y)
 extern "C" void AddScore(int howMany, int color);
 extern "C" void DisplayScoreAddPopup(Vec pos, int smallScoreID, int playerID, bool wantToShowThisForever);  // smallScoreID list is here -> https://note.com/reaz0n23/n/nb504e036da6d#143de39b-221f-4909-94f8-cc11e4dbdfdd
